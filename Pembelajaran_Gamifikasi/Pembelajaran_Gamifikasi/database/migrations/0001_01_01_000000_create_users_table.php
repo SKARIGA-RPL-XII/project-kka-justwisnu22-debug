2d9cbb37a71b306af37336415c8703e7f9e37c90
wisnu candra <justwisnu22@gmail.com>
2026-01-26 10:33:38 +0700

    Database

--- Pembelajaran_Gamifikasi/Pembelajaran_Gamifikasi/database/migrations/0001_01_01_000000_create_users_table.php
+++ Pembelajaran_Gamifikasi/Pembelajaran_Gamifikasi/database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,11 +13,12 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('username')->unique();
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->rememberToken();
+            $table->enum('role', ['user', 'admin'])->default('user');
+            $table->integer('level')->default(1);
+            $table->integer('exp')->default(0);
             $table->timestamps();
         });
 
